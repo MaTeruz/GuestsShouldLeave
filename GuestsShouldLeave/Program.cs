e67d5a798d8bedb650b9b29543644ad5ab801ce7
stuzb <stuzb@LAPTOP-7UV7VFFK>
2021-09-01 12:43:42 +0200

    Add project files.

---
+++ GuestsShouldLeave/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GuestsShouldLeave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Product> products = new Queue<Product>();
+
+            /*products.Enqueue(new Product() { ExpireDate = 020921, Name = "Milk", Price = 10 });
+            products.Enqueue(new Product() { ExpireDate = 090921, Name = "Meat oks", Price = 30 });
+            products.Enqueue(new Product() { ExpireDate = 090921, Name = "Meat pork", Price = 28 });
+            products.Enqueue(new Product() { ExpireDate = 120921, Name = "Bananas", Price = 12});
+            products.Enqueue(new Product() { ExpireDate = 200921, Name = "Apples", Price = 24 });
+            products.Enqueue(new Product() { ExpireDate = 150921, Name = "peanuts", Price = 8});*/
+
+            products.Enqueue(new Product(020921, "Milk", 10));
+            products.Enqueue(new Product(090921, "Meat oks", 30));
+            products.Enqueue(new Product(90921, "Meat Pork", 28));
+            products.Enqueue(new Product(20921, "bananas", 12));
+            products.Enqueue(new Product(200921,"Apples" , 24));
+            products.Enqueue(new Product(150921, "Peanuts", 8));
+
+            Console.WriteLine("\nAmount of products in storage {0}", products.Count);
+
+            foreach (Product product in products)
+            {
+                product.Print();
+            }
+
+            products.Dequeue();
+
+            Console.WriteLine("\nAmount of products in storage {0}", products.Count);
+
+            foreach (Product product in products)
+            {
+                product.Print();
+            }
+
+            int priceMax = 0;
+            int priceMin = 100;
+
+            foreach (Product product in products)
+            {
+                if (product.Price > priceMax)
+                {
+                    priceMax = product.Price;
+                }
+                if (product.Price < priceMin)
+                {
+                    priceMin = product.Price;
+                }
+            }
+
+            Console.WriteLine($"\nThe most expensive product is: {priceMax}");
+            Console.WriteLine($"\nThe cheapest product is: {priceMin}");
+
+
+            
+            Console.WriteLine("");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GuestsShouldLeave/product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestsShouldLeave
+{
+    class Product
+    {
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public int ExpireDate { get; set; }
+
+        public Product(int ExpireDate_, string Name_, int price_) 
+        {
+            Price = price_;
+            Name = Name_;
+            ExpireDate = ExpireDate_;
+
+        }
+        public void Print()
+        {
+            Console.WriteLine("Name: "+ Name);
+            Console.WriteLine("Expiretion date: " + ExpireDate);
+            Console.WriteLine("Price: " + Price);
+        }
+    }
+}
